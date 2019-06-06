f6863e8b1cd632cadd5c3bdaf828380eb84331ba
Kara Tong <karatts@users.noreply.github.com>
2019-06-06 18:11:11 -0400

    Update Stdin-Stdout.java

--- Basics/Stdin-Stdout.java
+++ Basics/Stdin-Stdout.java
@@ -15,3 +15,24 @@ public class Solution {
         scan.close();
     }
 }
+
+------------------------------------------------------------------------------------
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        Double d = scan.nextDouble();
+        String s = scan.nextLine();
+        while(s.equals("")){
+            
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);s = scan.nextLine();
+        }
+    }
+}
