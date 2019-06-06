9c42152dba9013cff4ebbe8046a17c355cc0a646
Kara Tong <karatts@users.noreply.github.com>
2019-06-06 18:12:34 -0400

    Update Stdin-Stdout.java

--- Basics/Stdin-Stdout.java
+++ Basics/Stdin-Stdout.java
@@ -33,7 +33,6 @@ public class Solution {
 
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
-        System.out.println("Int: " + i);s = scan.nextLine();
-        }
+        System.out.println("Int: " + i);
     }
 }
